2a7e05a709c84a0e5c18fb1338ed92f06cd6944f
khtran <codlove@gmail.com>
2013-03-30 19:24:14 +0000

    Refactor file structure

---
+++ src/main/java/com/wishop/dto/UserDetails.java
@@ -0,0 +1,25 @@
+package com.wishop.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class UserDetails {
+	@Id
+	private int userId;
+	private String username;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+}
