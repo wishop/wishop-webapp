74530f96d66f378e15980dbb49f12054206322ab
wishop <codlove@gmail.com>
2013-04-01 14:36:49 +0000

    Added Address embeddable object

---
+++ src/main/java/com/wishop/dto/Address.java
@@ -0,0 +1,44 @@
+package com.wishop.dto;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+	
+	private String country;
+	private String state;
+	private String city;
+	private String street;
+	private String postcode;
+	
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}	
+}
