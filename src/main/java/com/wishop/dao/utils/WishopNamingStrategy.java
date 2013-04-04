3d7749826c84383192d14b095674a385c555bfc5
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-04-04 23:57:59 +0100

    Changing the package name from com.wishop.dao.util to com.wishop.dao.utils

---
+++ src/main/java/com/wishop/dao/utils/WishopNamingStrategy.java
@@ -0,0 +1,28 @@
+package com.wishop.dao.utils;
+
+import org.hibernate.cfg.ImprovedNamingStrategy;
+
+/**
+ * <p>
+ * This class provides a feature that enforces naming standards automatically.
+ * All table names should follow the pattern <table_name>
+ * </p>
+ * 
+ * @author pmonteiro
+ * 
+ */
+public class WishopNamingStrategy extends ImprovedNamingStrategy {
+	private static final String USER_TABLE_PLURAL = "USERS";
+	private static final String USER_TABLE_SINGULAR = "USER";
+	private static final long serialVersionUID = 2514439297837969186L;
+	
+	/**
+	 * Because some classnames might have reserved DB keywords, we need to change them accordingly 
+	 */
+	public String classToTableName(String className) {
+		if (USER_TABLE_SINGULAR.equalsIgnoreCase(className)) {
+			className = USER_TABLE_PLURAL;
+		}
+		return super.classToTableName(className);
+	}
+}
