a9aefe7b7f36300295c1f02cef00945985c9b885
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-04-06 23:05:08 +0100

    Initial Commit

---
+++ src/main/java/com/wishop/utils/WishopMessagesConstants.java
@@ -0,0 +1,10 @@
+package com.wishop.utils;
+
+/**
+ * Interface that hold the WishopMessages constants, used to access the values inside the properties files.
+ * @author pmonteiro
+ *
+ */
+public interface WishopMessagesConstants {
+	
+}
