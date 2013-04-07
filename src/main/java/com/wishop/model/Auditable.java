5ad9c38360e42c9462acd1e2f7f57fa57474f5cc
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-04-07 18:36:05 +0100

    Adding Comments

--- src/main/java/com/wishop/model/Auditable.java
+++ src/main/java/com/wishop/model/Auditable.java
@@ -2,9 +2,35 @@ package com.wishop.model;
 
 import java.io.Serializable;
 
+/**
+ * This is the contract that all BaseObjects must implement
+ * @author pmonteiro
+ *
+ * @param <ID>
+ */
 public interface Auditable<ID extends Serializable> {
+	
+	/**
+	 * Return the BaseObject id
+	 * @return id
+	 */
 	public ID getId();
+	
+	/**
+	 * Check if the BaseObject is new. This is specially useful for the controllers
+	 * @return boolean
+	 */
 	public boolean isNew();
+	
+	/**
+	 * Set the Audit information
+	 * @param auditInfo
+	 */
 	public void setAuditInfo(AuditInfo<ID> auditInfo);
+	
+	/**
+	 * Get the Audit Information
+	 * @return
+	 */
     public AuditInfo<ID> getAuditInfo();
 }
