297bddc63ec42c2d32b08c70352b469a8313d602
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-04-07 01:32:32 +0100

    Refactoring the WishopApplicationContext

--- src/main/java/com/wishop/hibernate/SimpleCommand.java
+++ src/main/java/com/wishop/hibernate/SimpleCommand.java
@@ -8,31 +8,26 @@ import java.util.Date;
 import java.util.Iterator;
 
 import org.apache.log4j.Logger;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.wishop.dao.exceptions.HibernateSessionException;
 import com.wishop.model.User;
 import com.wishop.service.UserService;
+import com.wishop.utils.WishopApplicationContext;
 
 public class SimpleCommand {
 
 	private UserService userService;
 	//private RoleService cmsRoleService;
 	private Logger logger = Logger.getLogger(SimpleCommand.class);
-	private String [] applicationContexts;
+	
 	public void run() {
 		try{
-			applicationContexts = new String[] { 
-					"classpath:/META-INF/applicationContext.xml",  
-					"classpath:/META-INF/springApp/daoContext-app.xml" };
-			ApplicationContext context = new ClassPathXmlApplicationContext(applicationContexts);
-			userService = (UserService) context.getBean("userService");
-			//cmsRoleService = (ICmsRoleService) context.getBean("cmsRoleService");
+			userService = (UserService) WishopApplicationContext.getBean("userService");
+			//cmsRoleService = (RoleService) WishopApplicationContext.getBean("cmsRoleService");
 			populateDatabase();
 			displayDatabase();
 			//with cache
-			logger.info("WITH CACHE NOW!");
+			logger.info("NOW WITH CACHE!");
 			displayDatabase();
 		} catch (HibernateSessionException e) {
 			logger.error("Error", e);
@@ -52,29 +47,30 @@ public class SimpleCommand {
 		   logger.info("Name: " + user.getFullName());
 		   logger.info("DoB: " + user.getDateOfBirth());
 		   logger.info("Address Details:");
-		   //CmsAddress userCmsAddress = user.getCmsAddress();
-//		   if(userCmsAddress != null) {
-//			   logger.info("Address Line 1: " + userCmsAddress.getAddressLine1());
-//			   logger.info("Address Line 2: " + userCmsAddress.getAddressLine2());
-//			   logger.info("Address Line 3: " + userCmsAddress.getAddressLine3());
-//			   logger.info("City: " + userCmsAddress.getCity());
-//			   logger.info("County: " + userCmsAddress.getCounty());
-//			   logger.info("Postcode: " + userCmsAddress.getPostcode());
-//			   logger.info("--------");
-//		   }
 		}
 	}
 
 	private void populateDatabase() throws NoSuchAlgorithmException, ParseException {
-		User user = new User();
-		user.setFirstName("Paulo");
-		user.setLastName("Monteiro");
 		DateFormat formatter = new SimpleDateFormat("MM/dd/yy");
+
+		User user1 = new User();
+		user1.setFirstName("Paulo");
+		user1.setLastName("Monteiro");
 		Date date = formatter.parse("24/05/1982");
-		user.setDateOfBirth(date);
-		user.setEmail("paulo.from.portugal001@gmail.com");
-		user.setPassword("password");
-		user.setMobile("07590541213");
-		userService.saveOrUpdate(user);
+		user1.setDateOfBirth(date);
+		user1.setEmail("paulo.from.portugal@gmail.com");
+		user1.setPassword("password");
+		user1.setMobile("07590541213");
+		userService.saveOrUpdate(user1);
+		
+		User user2 = new User();
+		user2.setFirstName("Hoang");
+		user2.setLastName("Tran");
+		date = formatter.parse("24/06/1984");
+		user2.setDateOfBirth(date);
+		user2.setEmail("hoang.from.australia@gmail.com");
+		user2.setPassword("password");
+		user2.setMobile("1231231321323");
+		userService.saveOrUpdate(user2);
 	}
 }
--- src/main/java/com/wishop/utils/WishopApplicationContext.java
+++ src/main/java/com/wishop/utils/WishopApplicationContext.java
@@ -3,18 +3,11 @@ package com.wishop.utils;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class WishopApplicationContext implements ApplicationContextAware {
 	
-	private static WishopApplicationContext cmsApplicationContext;
 	private static ApplicationContext applicationContext;
-	
-	public static WishopApplicationContext getInstance() {
-		if(cmsApplicationContext == null) {
-			cmsApplicationContext = new WishopApplicationContext();
-		}
-		return cmsApplicationContext;
-	}
 	
 	/**
      * Sets the application context, so the bean becomes aware of the Spring context.
@@ -27,10 +20,26 @@ public class WishopApplicationContext implements ApplicationContextAware {
 	}
 	
 	/**
-     * Returns the ApplicationContext object, previously injected by Spring. 
-     * @return ApplicationContext
-     */
+	 * Method that retuns a bean from the Spring Application context
+	 * @param beanName
+	 * @return Proxy of bean
+	 */
+	public static Object getBean(String beanName) {
+		return getApplicationContext().getBean(beanName);
+	}
+
+	/**
+	 * Method that retrieves the Spring Application context
+	 * @return ApplicationContext
+	 */
 	public static ApplicationContext getApplicationContext() {
+		
+		if(applicationContext == null) {
+			String [] applicationContexts = new String[] { 
+					"classpath:/META-INF/applicationContext.xml",  
+					"classpath:/META-INF/springApp/daoContext-app.xml" };
+			applicationContext = new ClassPathXmlApplicationContext(applicationContexts);
+		}
 		return applicationContext;
 	}
 
