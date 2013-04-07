87172db361adbb2eed7b84b43d594faf219eb641
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-04-07 01:15:46 +0100

    Removing comments

--- src/main/java/com/wishop/hibernate/WishopMain.java
+++ src/main/java/com/wishop/hibernate/WishopMain.java
@@ -5,8 +5,5 @@ public class WishopMain {
 	public static void main(String[] args) {
 		SimpleCommand simpleCommand = new SimpleCommand();
 		simpleCommand.run();
-		//Md5PasswordEncoder encoder = new Md5PasswordEncoder();
-		//System.out.println("MD5 Hash (1): " + encoder.encodePassword("qwerty123", new String("pmonteiro@salmon.com")));
-		//System.out.println("MD5 Hash (26): " + encoder.encodePassword("123123", new String("paulo.from.portugal@gmail.com")));
 	}
 }
