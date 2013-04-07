02dd26d4359b6ae8f14d601fe5ec6aa6245ac930
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-04-07 02:28:53 +0100

    changing the java application to com.wishop.run

---
+++ src/main/java/com/wishop/run/WishopMain.java
@@ -0,0 +1,9 @@
+package com.wishop.run;
+
+
+public class WishopMain {
+	public static void main(String[] args) {
+		SimpleCommand simpleCommand = new SimpleCommand();
+		simpleCommand.run();
+	}
+}
