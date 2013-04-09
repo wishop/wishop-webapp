5c9d41888506ca6f5f2f020b5fb658c5ba95f90b
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-04-09 18:38:24 +0100

    Initial Commit

---
+++ src/test/java/com/wishop/service/BaseServiceTest.java
@@ -0,0 +1,69 @@
+package com.wishop.service;
+
+
+/**
+ * Contract for the jUnit BaseServiceTest
+ * 
+ * @author pmonteiro
+ * 
+ */
+public interface BaseServiceTest {
+
+	/**
+	 * Test the BaseService <code>getById(ID id)</code> method.<br>
+	 */
+	public void testGetById();
+
+	/**
+	 * Test the BaseService <code>getAll()</code> method.<br>
+	 */
+	public void testGetAll();
+
+	/**
+	 * Test the BaseService <code>getAll(Long id)</code> method.<br>
+	 */
+	public void testGetAllExceptOne();
+
+	/**
+	 * Test the BaseService <code>getAll(boolean delete)</code> method.<br>
+	 */
+	public void testGetAllByDeleteBasis();
+
+	/**
+	 * Test the BaseService <code>getAll(int firstResult, int maxResults)</code>
+	 * method.<br>
+	 */
+	public void testGetAllByPagination();
+
+	/**
+	 * Test the BaseService
+	 * <code>getAll(int firstResult, int maxResults, boolean deleted)</code>
+	 * method.<br>
+	 */
+	public void testGetAllByPaginationAndByDeleteBasis();
+
+	/**
+	 * Test the BaseService <code>save(Entity entity)</code> method.<br>
+	 */
+	public void testSave();
+
+	/**
+	 * Test the BaseService <code>update(Entity entity)</code> method.<br>
+	 */
+	public void testUpdate();
+
+	/**
+	 * Test the BaseService <code>saveOrUpdate(Entity entity)</code> method.<br>
+	 */
+	public void testSaveOrUpdate();
+
+	/**
+	 * Test the BaseService <code>delete(Entity entity)</code> method.<br>
+	 */
+	public void testDelete();
+
+	/**
+	 * Test the BaseService <code>purge(Entity entity)</code> method.<br>
+	 */
+	public void testPurge();
+}
---
+++ src/test/java/com/wishop/service/BaseServiceTestImpl.java
@@ -0,0 +1,12 @@
+package com.wishop.service;
+
+/**
+ * Contract for the jUnit BaseServiceTest
+ * 
+ * @author pmonteiro
+ * 
+ */
+public abstract class BaseServiceTestImpl implements BaseServiceTest {
+
+	
+}
