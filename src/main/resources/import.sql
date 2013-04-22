3947d14dc6dcc0f082659c3ac7e29140b5444c77
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-04-22 22:09:34 +0100

    Removing Large Objects

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -18,11 +18,11 @@ INSERT INTO audit_log_record (id, created, entity_class, entity_id, message, use
 
 
 --
--- Data for Name: hibernate_sequences; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: wishop_keys; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO hibernate_sequences (sequence_name, sequence_next_hi_value) VALUES ('users', 4);
-INSERT INTO hibernate_sequences (sequence_name, sequence_next_hi_value) VALUES ('audit_log_record', 4);
+INSERT INTO wishop_keys (table_name, next_value) VALUES ('users', 4);
+INSERT INTO wishop_keys (table_name, next_value) VALUES ('audit_log_record', 4);
 
 
 --
@@ -41,9 +41,9 @@ INSERT INTO hibernate_sequences (sequence_name, sequence_next_hi_value) VALUES (
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO users (id, creation_timestamp, creator_user_id, modification_timestamp, modifier_user_id, deleted, account_active, address_line1, address_line2, address_line3, city, country, county, postcode, date_of_birth, email, fax, first_name, last_name, mobile, password, profile, telephone, title) VALUES (1, '2013-04-07 18:29:17.519', -1, '2013-04-07 18:29:17.519', -1, false, true, '4 Wensleydale Drive', NULL, NULL, 'Leeds', 'UK', 'West Yorkshire', 'LS12 2HU', '1983-12-05 00:00:00', 'paulo.from.portugal@gmail.com', NULL, 'Paulo', 'Monteiro', '07590541213', '3fb5fce3c24aa7e9d1a01dc1aa43abf3', '35758', NULL, 'Mr');
-INSERT INTO users (id, creation_timestamp, creator_user_id, modification_timestamp, modifier_user_id, deleted, account_active, address_line1, address_line2, address_line3, city, country, county, postcode, date_of_birth, email, fax, first_name, last_name, mobile, password, profile, telephone, title) VALUES (2, '2013-04-07 18:29:19.55', -1, '2013-04-07 18:29:19.55', -1, false, true, '2 Winston House, Endsleigh St', NULL, NULL, 'London', 'UK', 'London', 'WC1H 0EA', '1985-12-06 00:00:00', 'tran.k.hoang@gmail.com', NULL, 'Hoang', 'Tran', '1231231321323', 'f284baa8483b93f498aec72cc43be5b0', '35759', NULL, NULL);
-INSERT INTO users (id, creation_timestamp, creator_user_id, modification_timestamp, modifier_user_id, deleted, account_active, address_line1, address_line2, address_line3, city, country, county, postcode, date_of_birth, email, fax, first_name, last_name, mobile, password, profile, telephone, title) VALUES (3, '2013-04-07 18:33:40.379', -1, '2013-04-07 18:33:40.379', -1, false, true, 'Test Address', NULL, NULL, 'London', 'UK', 'London', 'SE1', '2014-12-01 00:00:00', 'test.user@mailinator.com', NULL, 'Test', 'User', '1231231231', '0dcf2102d8bc6d598cbc6cf2892cffc3', '35833', NULL, 'Mr');
+INSERT INTO users (id, creation_timestamp, creator_user_id, modification_timestamp, modifier_user_id, deleted, account_active, address_line1, address_line2, address_line3, city, country, county, postcode, date_of_birth, email, fax, first_name, last_name, mobile, password, profile, telephone, title) VALUES (1, '2013-04-07 18:29:17.519', -1, '2013-04-07 18:29:17.519', -1, false, true, '4 Wensleydale Drive', NULL, NULL, 'Leeds', 'UK', 'West Yorkshire', 'LS12 2HU', '1983-12-05 00:00:00', 'paulo.from.portugal@gmail.com', NULL, 'Paulo', 'Monteiro', '07590541213', '3fb5fce3c24aa7e9d1a01dc1aa43abf3', NULL, NULL, 'Mr');
+INSERT INTO users (id, creation_timestamp, creator_user_id, modification_timestamp, modifier_user_id, deleted, account_active, address_line1, address_line2, address_line3, city, country, county, postcode, date_of_birth, email, fax, first_name, last_name, mobile, password, profile, telephone, title) VALUES (2, '2013-04-07 18:29:19.55', -1, '2013-04-07 18:29:19.55', -1, false, true, '2 Winston House, Endsleigh St', NULL, NULL, 'London', 'UK', 'London', 'WC1H 0EA', '1985-12-06 00:00:00', 'tran.k.hoang@gmail.com', NULL, 'Hoang', 'Tran', '1231231321323', 'f284baa8483b93f498aec72cc43be5b0', NULL, NULL, NULL);
+INSERT INTO users (id, creation_timestamp, creator_user_id, modification_timestamp, modifier_user_id, deleted, account_active, address_line1, address_line2, address_line3, city, country, county, postcode, date_of_birth, email, fax, first_name, last_name, mobile, password, profile, telephone, title) VALUES (3, '2013-04-07 18:33:40.379', -1, '2013-04-07 18:33:40.379', -1, false, true, 'Test Address', NULL, NULL, 'London', 'UK', 'London', 'SE1', '2014-12-01 00:00:00', 'test.user@mailinator.com', NULL, 'Test', 'User', '1231231231', '0dcf2102d8bc6d598cbc6cf2892cffc3', NULL, NULL, 'Mr');
 
 
 --
